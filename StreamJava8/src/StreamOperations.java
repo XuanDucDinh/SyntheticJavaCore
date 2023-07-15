4c96d95272829b3db3b39f6bc427ce248939715e
XuanDucDinh <ducdx2712@gmail.com>
2023-07-15 18:21:15 +0700

    stream: add new file

---
+++ StreamJava8/src/StreamOperations.java
@@ -0,0 +1,28 @@
+public class StreamOperations {
+    public static void main(String[] args) {
+        Integer option = 0;
+        switch (option) {
+            case  0 :
+            {
+              break;
+            }
+            case  1 :
+            {
+                break;
+            }
+            case  2 :
+            {
+                break;
+            }
+            case  3 :
+            {
+                break;
+            }
+            case  4 :
+            {
+                break;
+            }
+            default:
+        }
+    }
+}
